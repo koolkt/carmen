a108dd4ce21828e8abc340ba5d4a5bf5a25ae75d
koolkat <adolfo.flores@epitech.eu>
2017-07-12 18:29:54 +0200

    add petal expansion effect with info

--- app/assets/js/wall.js
+++ app/assets/js/wall.js
@@ -15,19 +15,21 @@ function log() {
     console.log(...arguments);
 }
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+/* function getRandomInt(min, max) {
+ *     return Math.floor(Math.random() * (max - min + 1)) + min;
+ * }
+ * */
 
 function range(n) {
     return [...Array(n).keys()];
 }
 
 const testJson = (amount) => ({
-    first_name: "Anonyme",
+    first_name: "Georges",
     last_name: "Ssssssss",
     amount,
-    anonymous: "f"
+    anonymous: "f",
+    country: 'FR',
 });
 
 function generateTestData() {
@@ -56,6 +58,17 @@ function partition(elems, cSize) {
     return range(Math.ceil(elems.length/cSize)).map(i => elems.slice(cSize*i, (cSize*i) + cSize));
 }
 
+function createText(x, y, fontSize, petalText) {
+    var newText = document.createElementNS("http://www.w3.org/2000/svg","text");
+    newText.setAttributeNS(null,"x",x);
+    newText.setAttributeNS(null,'y',y); 
+    newText.setAttributeNS(null,'font-size', fontSize);
+    newText.setAttributeNS(null,'font-weight', 'bold');
+    newText.setAttributeNS(null,'fill','white');
+    newText.appendChild(document.createTextNode(petalText))
+    return newText;
+}
+
 function renderDonnors (donors) {
     const columns = getColumns();
     const wall = document.getElementById('donnors-wall');
@@ -63,15 +76,25 @@ function renderDonnors (donors) {
         const isBig = {};
         const petalElements = donors.map(d => {
             const petal = createNodeFromStr(petals[getPetalNumberFromDonation(d.amount)]).firstElementChild;
-            petal.id = getRandomInt(1, 100000);
+            petal.id = `${getRandomInt(1, 1000)}-${getRandomInt(1000, 100000)}`;
+            petal.style.zIndex = 1;
             petal.addEventListener('click', (e) => {
                 console.log(e.srcElement.nodeName)
-                const elem = e.srcElement.nodeName === 'path' ? e.srcElement.parentElement : e.srcElement;
+                const elem = e.srcElement.nodeName === 'path' || e.srcElement.nodeName === 'text' ? e.srcElement.parentElement : e.srcElement;
                 if(isBig[elem.id]) {
-                    elem.style.transform =  'scale(1)';
+                    elem.style.transform = 'scale(1)';
+                    elem.style.zIndex = 1;
                     isBig[elem.id] = false;
                 } else {
+                    const nameString = `${d.first_name} ${d.last_name}`;
+                    const donnorName = createText(32, 43, 6, d.anonymous  === 't' ? 'Anonymous' : nameString);
+                    const amount = createText(48, 60, 6, `€ ${d.amount}`);
+                    const country = createText(54, 85, 6, d.country);
+                    elem.appendChild(donnorName);
+                    elem.appendChild(amount);
+                    elem.appendChild(country);
                     elem.style.transform =  'scale(7)';
+                    elem.style.zIndex = 1000;
                     isBig[elem.id] = true;
                 }
             }, false)
