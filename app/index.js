a9eedd98c0edc50aaaf8ecd0fb7ea84179a67ef4
koolkat <adolfo.flores@epitech.eu>
2017-08-21 12:13:45 +0200

    update

--- app/index.js
+++ app/index.js
@@ -5,8 +5,26 @@ import initImages from './assets/js/images';
 import initVideoButtons from './assets/js/video';
 import initWall from './assets/js/wall';
 
+function doInLocation(locationToFunction) {
+    const parser = document.createElement('a');
+    parser.href = window.location.href;
+    const uri = parser.pathname.split('/').slice(-1)[0].split('.')[0] || '/';
+    const defaultFunc = _ => console.log(`Inside unknown page: ${uri}`);
+    const func = locationToFunction[uri] || defaultFunc;
+    func(uri);
+}
+
 function init () {
-    initTransaltions();
+    const logUri = (testUri) => (uri) => console.log(`Inside ${testUri}: ${uri}`)
+    const setMenuColor = (menuId) => _ => (document.getElementById(menuId) || {style: {color: ''}}).style.color = '#8f1a24'
+    const locationToFunction = {
+        'about': setMenuColor('lamaisonMenu'),
+        'donors-wall': setMenuColor('donorsWallMenu'),
+        'campaigne': setMenuColor('campaigneMenu'),
+        '/': logUri('home'),
+    };
+    doInLocation(locationToFunction);
+    /* initTransaltions();*/
     initImages();
     initVideoButtons();
     initWall();
